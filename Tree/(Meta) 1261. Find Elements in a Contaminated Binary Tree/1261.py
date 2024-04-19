e631cf2dfe1231dfbcc96dbbe6ba4434a08afef5
Jacky Hung <jacky5559tw@gmail.com>
2024-04-19 22:13:45 +0100

    1261

---
+++ Tree/(Meta) 1261. Find Elements in a Contaminated Binary Tree/1261.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.root.val = 0
+        self.set = set()
+        
+        def dfs(root):
+            self.set.add(root.val)
+            if root.left:
+                root.left.val = 2 * root.val + 1
+                dfs(root.left)
+            if root.right:
+                root.right.val = 2 * root.val + 2
+                dfs(root.right)
+
+        dfs(root)
+
+    def find(self, target: int) -> bool:
+        return True if target in self.set else False
+
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
