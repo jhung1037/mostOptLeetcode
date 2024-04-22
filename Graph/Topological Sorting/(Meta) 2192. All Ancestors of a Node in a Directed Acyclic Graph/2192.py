0b4e80a6645beda0d0d34b34d0bb03092f26b121
Jacky Hung <jacky5559tw@gmail.com>
2024-04-22 21:26:33 +0100

    2192

---
+++ Graph/Topological Sorting/(Meta) 2192. All Ancestors of a Node in a Directed Acyclic Graph/2192.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+        for u, v in edges:
+            adj[u].append(v)
+            indegree[v] += 1
+
+        check = []
+        for i in range(n):
+            if indegree[i] == 0:
+                check.append(i)
+
+        res = [set() for _ in range(n)]
+        while check:
+            root = check.pop()
+            for child in adj[root]:
+                res[child].add(root)
+                res[child].update(res[root])
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    check.append(child)
+
+        for i in range(n):
+            res[i] = sorted(list(res[i]))
+        return res
