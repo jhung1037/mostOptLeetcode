14dc0fee0da3cb8df91c0b0d06756922d012e2ce
Jacky Hung <jacky5559tw@gmail.com>
2024-04-22 09:45:35 +0100

    207

---
+++ Graph/Topological Sorting/(Meta) 207. Course Schedule/(Meta)/207.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        adj = {i : set() for i in range(numCourses)}
+        indegree = [0] * numCourses
+        for prerequisite in prerequisites:
+            u, v = prerequisite
+            adj[u].add(v)
+            indegree[v] += 1
+        
+        visited = set()
+        check = []
+        for u, d in enumerate(indegree):
+            if d == 0:
+                check.append(u)
+        
+        while check:
+            root = check.pop()
+            visited.add(root)
+            for v in adj[root]:
+                if v in visited: return False
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    check.append(v)
+
+        return True if len(visited) == numCourses else False
