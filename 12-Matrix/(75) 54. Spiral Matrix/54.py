b572c5ccd2fa361670f3a1bfefae79726c700d9e
Jacky Hung <jackyhung@192.168.1.121>
2025-06-26 22:30:08 +0100

    54

--- 12-Matrix/(75) 54. Spiral Matrix/54.py
+++ 12-Matrix/(75) 54. Spiral Matrix/54.py
@@ -1,25 +1,25 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        rowZero = False
-        for r in range(ROWS):
-            for c in range(COLS):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    if r > 0:
-                        matrix[r][0] = 0
-                    else:
-                        rowZero = True
-        
-        for r in range(1, ROWS):
-            for c in range(1, COLS):
-                if matrix[r][0] == 0 or matrix[0][c] == 0:
-                    matrix[r][c] = 0
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        t, l, b, r = 0, 0, len(matrix)-1, len(matrix[0])-1
 
-        if matrix[0][0] == 0:
-            for r in range(ROWS):
-                matrix[r][0] = 0
+        res = []
+        while t <= b and l <= r:
+            for i in range(l, r+1):
+                res.append(matrix[t][i])
+            t+=1
 
-        if rowZero:
-            for c in range(COLS):
-                matrix[0][c] = 0
+            for i in range(t, b+1):
+                res.append(matrix[i][r])
+            r-=1
+            
+            if not (t <= b and l <= r): break
+
+            for i in range(r, l-1, -1):
+                res.append(matrix[b][i])
+            b-=1
+
+            for i in range(b, t-1, -1):
+                res.append(matrix[i][l])
+            l+=1
+
+        return res
