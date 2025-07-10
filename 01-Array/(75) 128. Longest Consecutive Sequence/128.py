cc4ed328548a13c4bd9285b0668b27e283d4baa8
Jacky Hung <jackyhung@192.168.1.152>
2025-07-10 22:19:51 +0100

    128

--- 01-Array/(75) 128. Longest Consecutive Sequence/128.py
+++ 01-Array/(75) 128. Longest Consecutive Sequence/128.py
@@ -2,15 +2,13 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         # time complexity: O(n)
         # space complexity: O(n)
-        dic = {}
-        maxi = 0
-        for num in nums:
-            if num in dic: continue
-            upper = dic[num + 1][1] if num + 1 in dic else num
-            lower = dic[num - 1][0] if num - 1 in dic else num
-            dic[num] = [lower, upper]
-            dic[upper][0] = lower
-            dic[lower][1] = upper
-            maxi = max(upper - lower + 1, maxi)
-
-        return maxi
+        res = 0
+        hmap = set(nums)
+        for num in hmap:
+            if num-1 not in hmap:
+                length = 1
+                while num+length in hmap:
+                    length += 1
+                res = max(res, length)
+        
+        return res
